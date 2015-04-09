b8f827570a21145c98460fb4b96d732d2afbc953
James Talmage <james@talmage.io>
2015-04-09 15:08:28 -0400

    add a synchronous version

--- fixture/combine.js
+++ fixture/combine.js
@@ -1,9 +1,22 @@
-require('../')({
+require('../')(
+  {
+    pattern:'coverage/*/coverage-final.json',
+    reporters:{
+      html:{}
+    }
+  },
+  function(err){
+    if(err) throw err;
+    console.log("***********ASYNC COMPLETE*********");
+  }
+);
+
+console.log("*********STARTING SYNC**************");
+
+require('../').sync({
   pattern:'coverage/*/coverage-final.json',
   reporters:{
     html:{}
   }
-})(function(err){
-  if(err) throw err;
-  console.log("***********COMPLETE*********");
 });
+console.log("***********SYNC COMPLETE**************");
--- index.js
+++ index.js
@@ -5,8 +5,7 @@ var readFile = Q.denodeify(fs.readFile);
 var Report = require('istanbul').Report;
 var Collector = require('istanbul').Collector;
 
-
-function makeReporters(options) {
+function makeReporterDefs(options) {
   var result = [];
   var reporters = options.reporters &&
   typeof options.reporters === 'object' ? options.reporters : {};
@@ -22,7 +21,7 @@ function makeReporters(options) {
 
   var append = function(t) {
     if(t && !reporters[t]) {
-      result.push({ type : t, options : options});
+      result.push({type : t, options : options});
       reporters[t] = true;
     }
   };
@@ -43,54 +42,84 @@ function makeReporters(options) {
   return result;
 }
 
-function makeCoverageTaskFn(opts){
-  opts = opts || {};
-  if (!opts.dir) opts.dir = 'coverage';
-  var pattern = opts.pattern || 'coverage/*-coverage.json';
-
-  return function (done){
-
-    var fileContentPromises = [];
-    var collector = new Collector();
-    var readDefer = Q.defer();
-
-    glob(pattern)
-      .on('match', function(filePath) {
-        fileContentPromises.push(readFile(filePath,'utf-8').then(function(fileContents){
-          collector.add(JSON.parse(fileContents));
-          return true;
-        }));
-      })
-      .on('error', readDefer.reject)
-      .on('end', function() {
-        Q.all(fileContentPromises).then(readDefer.resolve);
-      });
-
-    return readDefer.promise.then(
-      function(files) {
-        var writePromises = [];
-        makeReporters(opts).forEach(function(reporterDef) {
-          var defer = Q.defer();
-          Report.create(reporterDef.type, reporterDef.options)
-            .on('done', defer.makeNodeResolver())
-            .on('error', defer.makeNodeResolver())
-            .writeReport(collector, false);
-          writePromises.push(defer.promise);
-        });
-
-        return Q.all(writePromises);
-      }
-    ).then(
-      function(result){
-        if(done) done();
-        return true;
-      },
-      function(reason) {
-        if(done) done(reason);
-        return Q.reject(reason);
-      }
-    );
-  }
+function forEachReporter(reporterDefs, cb) {
+  reporterDefs.forEach(function(reporterDef){
+     cb(Report.create(reporterDef.type, reporterDef.options));
+  });
 }
 
-module.exports = makeCoverageTaskFn;
+function writeCombinedReports(reporterDefs, collector) {
+  var writePromises = [];
+  forEachReporter(reporterDefs, function(report) {
+    var defer = Q.defer();
+    writePromises.push(defer.promise);
+    report
+      .on('done', defer.makeNodeResolver())
+      .on('error', defer.makeNodeResolver())
+      .writeReport(collector, false);
+  });
+  return Q.all(writePromises);
+}
+
+function writeCombinedReportsSync(reporterDefs, collector) {
+  forEachReporter(reporterDefs, function(reporter) {
+    reporter.writeReport(collector, true);
+  });
+}
+
+function collectInputReports(pattern) {
+  var collector = new Collector();
+  var fileContentPromises = [];
+  var collectorDefer = Q.defer();
+
+  glob(pattern)
+    .on('match', function(filePath) {
+      fileContentPromises.push(readFile(filePath,'utf-8').then(function(fileContents){
+        collector.add(JSON.parse(fileContents));
+        return true;
+      }));
+    })
+    .on('error', collectorDefer.reject)
+    .on('end', function() {
+      Q.all(fileContentPromises).then(function(){
+        collectorDefer.resolve(collector);
+      });
+    });
+  return collectorDefer.promise;
+}
+
+function collectInputReportsSync(pattern){
+  var collector = new Collector();
+  glob.sync(pattern).forEach(function(file){
+    collector.add(JSON.parse(fs.readFileSync(file, 'utf-8')));
+  });
+  return collector;
+}
+
+function normalizeOpts(opts) {
+  opts = opts || {};
+  if (!opts.dir) opts.dir = 'coverage';
+  if (!opts.pattern) opts.pattern = 'coverage/*-coverage.json';
+  return opts;
+}
+
+function run(opts, done) {
+  opts = normalizeOpts(opts);
+  var reporters = makeReporterDefs(opts);
+  var promise = collectInputReports(opts.pattern).then(
+    function(collector) {
+      return writeCombinedReports(reporters, collector);
+    }
+  );
+  return Q.nodeify(promise, done);
+}
+
+function sync(opts) {
+  opts = normalizeOpts(opts);
+  var reporters = makeReporterDefs(opts);
+  var collector = collectInputReportsSync(opts.pattern);
+  writeCombinedReportsSync(reporters, collector);
+}
+
+module.exports = run;
+module.exports.sync = sync;
